d8d4ef47a1d066edad9359aa1e181dde2f2c3127
IDMS JPPP <95725607+IDMSJPPP@users.noreply.github.com>
2023-07-13 15:58:44 +0800

    updated

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React , {useState} from 'react'
+import bookhero from '../static/images/bookshero.jpg'
+import books from '../books.json'
 
 function Homepage() {
+  const [goodbooks] = useState(books);
+
   return (
+    // <div>
+  
+    //   <div>
+    //     <img className="bookshero mt-5" src={bookhero} alt="bookhero"/>
+    //     <div>
+    //       <h1 className="titleborder d-flex justify-content-center p-3">Zakky's BookShop</h1>
+    //     </div>
+        
+    //   </div>
+      
+    // </div>
+
     <div>
-      <h1>Homepage</h1>
+      <div className="bookshop-container">
+        <img className="bookshero mt-5" src={bookhero} alt="bookhero" />
+        <div className="bookshop-title">
+          <h1 className="titleborder d-flex justify-content-center p-3" style={{fontSize:"4rem"}}>Zakky's Book Library</h1>
+        </div>
+      </div>
+      <div className="mt-3">
+      <div className="row  d-flex justify-content-around">
+        <h2 className="d-flex justify-content-center mt-3">Some Of Our Books</h2>
+      
+        {goodbooks.slice(0, 3).map((goodbook) => {
+          return (
+            <div className="col-md-3 mt-3 pb-5">
+            <div class="card_border">
+             <img className="card-image" src={goodbook.imageLink} alt="Avatar"/>
+               <div className="container mt-3">
+                <h5 className="d-flex justify-content-center" >Author: {goodbook.author}</h5>
+                <h5 className="d-flex justify-content-center">Title: {goodbook.title}</h5>
+
+            </div>
+          </div>
+          </div>
+          )})}
+
+                  
+
+                            
+           
+          </div>
+
+      </div>
     </div>
   )
 }
